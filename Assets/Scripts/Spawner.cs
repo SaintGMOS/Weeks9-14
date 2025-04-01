879abf1d33c5ca782ea9a8e87ed6ecbb6b023c92
MSANTOS\gmosa <gmosantos@outlook.com>
2025-04-01 18:01:48 -0400

    Fixed what was not working which is the image mix up with the spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-   
+    public GameObject prefab; // Package Prefab
+    public GameObject spawnLocation; // Spawn Location of the Package
+    private GameObject package; // Ref to the currently spawned Package(Grabs the prefab building having its movement script)
+    public float destroyTime;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,4 +21,17 @@ public class Spawner : MonoBehaviour
        
     
     }
+
+
+
+    public void Spawn()
+    { 
+        package = Instantiate(prefab, spawnLocation.transform.position, Quaternion.identity);
+        
+    
+    
+    
+    }
+
+
 }
